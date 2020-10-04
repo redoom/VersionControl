fa2284c1d3fcf4d6988167e858b0d5039ecb293b
Daniel Illes <dani.illes96@gmail.com>
2020-10-04 22:26:46 +0200

    task 6 done

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace week4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            CreateExcel();
+        }
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+        private void CreateExcel()
+        {
+            string[] headers = new string[] {
+            "Kód",
+            "Eladó",
+            "Oldal",
+            "Kerület",
+            "Lift",
+            "Szobák száma",
+            "Alapterület (m2)",
+            "Ár (mFt)",
+            "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[i+1, i+1] = headers[i];
+            }
         }
     }
 }

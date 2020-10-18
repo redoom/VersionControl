43e0c70fb3538687984b1ef57b8a01dc034d4a2c
Daniel Illes <dani.illes96@gmail.com>
2020-10-18 12:33:12 +0200

    Task 4

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -8,12 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using week6.MnbServiceReference;
+using week6.Entities;
 
 namespace week6
 {
 
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
@@ -29,6 +31,8 @@ namespace week6
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            dataGridView1.DataSource = Rates;
         }
     }
 }

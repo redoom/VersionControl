9585b368378c93af8913d28816953c395896f894
Daniel Illes <dani.illes96@gmail.com>
2020-10-18 12:23:31 +0200

    Task2

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week6.MnbServiceReference;
 
 namespace week6
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+
         }
     }
 }

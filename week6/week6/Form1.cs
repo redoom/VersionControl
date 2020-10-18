f86664d03a73c20e028c7a6303b45021b21916bc
Daniel Illes <dani.illes96@gmail.com>
2020-10-18 12:23:54 +0200

    Task3

--- week6/week6/Form1.cs
+++ week6/week6/Form1.cs
@@ -18,7 +18,17 @@ namespace week6
         {
             InitializeComponent();
 
+            var mnbService = new MNBArfolyamServiceSoapClient();
 
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
